dd36169ab1554476c35f8f4a6aab13c6cda65076
Venu <pvenumohan004@gmail.com>
2026-08-18 15:52:57 +0530

    Add configurable exam marks and negative marking

--- templates/exams/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Exam
-
-
-@admin.register(Exam)
-class ExamAdmin(admin.ModelAdmin):
-    list_display = ('title', 'subject', 'difficulty', 'duration_minutes', 'created_by')
-    search_fields = ('title', 'subject')
-    list_filter = ('difficulty', 'subject')
